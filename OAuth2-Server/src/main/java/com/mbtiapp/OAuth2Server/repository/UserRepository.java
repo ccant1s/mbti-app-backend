f8d4c31073116f35fbb37a988351b231580b7bf2
ccant1s <clint.plantones11@gmail.com>
2023-05-21 23:45:05 +0800

    OAuth2 Initial Commit (client and provider added)

---
+++ OAuth2-Server/src/main/java/com/mbtiapp/OAuth2Server/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mbtiapp.OAuth2Server.repository;
+
+import com.mbtiapp.OAuth2Server.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
